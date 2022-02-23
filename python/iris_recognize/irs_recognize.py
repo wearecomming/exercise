c0c3b6ea2b79b1b18bc1f2917cd39b899ae34c41
wearecomming <1830653023@qq.com>
2022-02-23 18:46:58 +0800

    load

--- python/iris_recognize/irs_recognize.py
+++ python/iris_recognize/irs_recognize.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-import train
+import joblib
 
 
 x = []
@@ -9,5 +9,6 @@ x=input().split( )
 xx = []
 xx.append(x)
 xx = np.array( xx,dtype=float )
-print(train.knn.predict(xx))
+knn = joblib.load("E:\exercise\python\iris_recognize\knn.pkl")
+print(knn.predict(xx))
 
--- python/iris_recognize/train.py
+++ python/iris_recognize/train.py
@@ -1,8 +1,10 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import joblib
 import numpy as np
 import dataload
 
 
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(dataload.x_train,dataload.y_train)
+joblib.dump(knn,"E:\exercise\python\iris_recognize\knn.pkl")
