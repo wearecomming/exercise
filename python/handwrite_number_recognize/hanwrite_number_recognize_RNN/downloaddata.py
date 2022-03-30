7f6c19246cac6af5f32a69c771c5aa129d3c54dd
wearecomming <1830653023@qq.com>
2022-03-30 16:11:31 +0800

    work2

---
+++ python/handwrite_number_recognize/hanwrite_number_recognize_RNN/downloaddata.py
@@ -0,0 +1,23 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+
+batch_size_train = 1000
+batch_size_test = 1000
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./data/', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_train, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./data/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
+
